b8cfa963a1cfbb95ed58c5f14f20b68302912dc8
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-05-14 09:25:50 -0400

    still unclear with MVC

--- js/app.js
+++ js/app.js
@@ -19,11 +19,63 @@ Controllers: action manager it can send commands to the model to update its stat
 alert("I'm alive!")
 //debugger;
 
+//I can _see tasks_!
+
 document.head.parentElement.className = "js"; // It's alive!
 
 //var elements = document.querySelectorAll('h3');
 // lets start by defining a variable and play with what we feed to .querySelector?
-// header,
+// header (h3: in this case), find with the element inspector the
 
-var elements = document.querySelectorAll('h3');
+//function from Nesting accordions?
+
+// function( $, window, undefined ) {
 //
+// 	'use strict';
+//
+// 	// global
+// 	var $body = $( 'html, body' );
+//
+// 	$.CBPNTAccordion = function( options, element ) {
+// 		this.$el = $( element );
+// 		this._init( options );
+// 	};
+
+//
+
+var elements = document.querySelectorAll('header');
+
+/*use [`Element.addEventListener`](https://developer.mozilla.org/en-US/docs/
+Web/API/EventTarget/addEventListener) to attach event listeners
+
+
+I can _add tasks_!
+
+I can _complete tasks_!
+
+I can _delete tasks_!
+
+I can _edit tasks_! */
+
+
+
+//Found a helpful (hopefully): http://spinejs.com/docs/example_tasks
+
+
+
+class TaskApp extends Spine.Controller
+  elements:
+    ".items": "items"
+
+  constructor: ->
+    super
+    Task.bind("create",  @addOne)
+    Task.bind("refresh", @addAll)
+    Task.fetch()
+
+  addOne: (task) =>
+    view = new Tasks(item: task)
+    @items.append(view.render().el)
+
+  addAll: =>
+    Task.each(@addOne)
