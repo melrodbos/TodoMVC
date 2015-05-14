fc0bd363de9c49ba7454045d7eb7ce78d995cb74
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-05-14 15:42:30 -0400

    trying to get something tested, correcting syntax

--- js/app.js
+++ js/app.js
@@ -1,12 +1,99 @@
+//Copied the following documentation from the class repo (just in case I had any typos):
+
+
+//debugger;
 (function (window) { // IIFE: Immediately Invoked Function Expression
 	'use strict'; // For bonus points, what does this do?
 
-	var list = [ ];
+	// CONTROLLER FOR: I can add a task to my list...
+	// 1. What event should I be listening for? keydn, keyup, keypress
+	// 2. What element makes sense to listen for that event? input.new-todo
+	// 3. What do I need to do when that event fires?
 
-	console.log(list);
+// 	// GIVEN an HTML element <input class="new-todo">
+// 	var newTodoInput = document.querySelector('input.new-todo');
+//
+// 	// WHEN the user types a task
+// 	newTodoInput.addEventListener('keyup', function addTodoController(event){
+// 		if ( event.keyCode === 13 ){ // AND presses the "Enter" key
+// 			console.log('sanity check!');
+// 			// THEN:
+// 			// TODO Save the thing to remember (task) to the list of things to remember (taskList)
+// 			// TODO Remove the thing to remember (task) from the "What needs to be done?" box (input.new-todo)
+// 			// TODO Update the number of tasks in the footer...
+// 			// TODO Add a new task (ul.todo-list > li) to the list of tasks _in the display_ (ul.todo-list)
+// 		}
+// 	}); // END addEventListener(addTodoController)
+// })(window);
 
-	todos.addTaskToList("Remember the milk", list);
+//
 
-	console.log(list);
+
+var newTodoInput = document.querySelector('input.new-todo');
+
+// WHEN the user types a task
+newTodoInput.addEventListener('keyup', function addTodoController(event){
+	if ( event.keyCode === 13 ){ // AND presses the "Enter" key
+		var task = document.querySelector('inout.new-todo').value;
+
+		todos.taskList.push(task);
+
+		console.log(todos.taskList);
+		// THEN:
+		// TODO Save the thing to remember (task) to the list of things to remember (taskList)
+		// TODO Remove the thing to remember (task) from the "What needs to be done?" box (input.new-todo)
+		// TODO Update the number of tasks in the footer...
+		// TODO Add a new task (ul.todo-list > li) to the list of tasks _in the display_ (ul.todo-list)
+	}); // END addEventListener(addTodoController)
 
 })(window);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // My version:
+// (function (window) { //IIFE: Immediately invoke dfn expression
+// 	'use strict'; //what does this do??
+// 	debugger;
+// // CONTROLLER: I can add a task to my list...
+// // 1. What event shoul I be listening for? keydn, keyup, keypress
+// // 2. What element makes sense to isten to that event? input.new-todo
+// // 3.What do I need to do when that event fires?
+//
+// // GIVEN an HTML element <input class= "new-todo">
+// // addEventListener('keydn', function aNameInHere (){
+// var newTodoInput = document.querySelector('input.new-todo');
+//
+//
+//
+// // WHEN: the user types a task AND presses the "ENTER" key
+// // newTodoInput.addEventListener('keyup', function(){ console.log(arguments); });
+// // newTodoInput.addEventListener('keydn', function addTodoController (){
+// 	// TO ADD A NEW TASK:
+//
+//
+// 	// 1. Click on "What needs to be done?" (input class = "new-todo")
+// 	// 2. Type the task: "Remember the milk"
+// 	// 3. Press the :"Enter" key
+// 	// 4. ROBOT: Save the thing to remember ("task") to the list of things to remember (taskList)
+// 	// 5. ROBOT: Remove the thing to remember (task) from the "What needs to be done?" box ()
+// 	// 6. ROBOT: Update the number of tasks in the footer...
+// 	// 7. ROBOT: Add a new task (ul.todoList > li)to the list of tasks_in_the_display_(ul.toggle)
+//
+
+
+// }
+
+// // });
