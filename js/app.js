3825e8c8889300e9fa256a5254256502d07af372
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-05-13 23:55:02 -0400

    Comments to understand

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
-(function (window) {
-	'use strict';
+// (function (window) {
+// 	'use strict';
+//
+// 	// Your starting point. Enjoy the ride!
+//
+// })(window);
 
-	// Your starting point. Enjoy the ride!
+//Trying to get an understanding on what the MVC is...
+//Model:
+//View:
+//Controllers:
 
-})(window);
+/*I am using the nestingAccordions file as reference to try to get something done?
+Confusssion cleared? The MVC works with files:
+Model: data storage
+View: the part of JS that we see in the html file.
+Controllers: action manager */
+
+document.head.parentElement.className = "js"; // It's alive!
