df6228fe3a2ff9c835c2ca28dd2d77e028552e47
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-05-14 20:21:07 -0400

    syntax error checking while transferring code from master branch commits

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-(function (window) {
+(function(window) {
     'use strict'; // For bonus points what does this do?
-// debugger;
+    // debugger;
 
 
     //  CONTROLLER FOR: I can ada a task to my list...
@@ -8,47 +8,58 @@
     //  2. what element makes since to listen for that event? input.new-todo
     //  3. What do I need to do when that event fires?
 
-                    // one way to write the query selector
-                        // document.querySelector('input.new-todo');
-                        // .addEventListener('keydn', function addTodoController(){
-                        // })
-        var newTodoInput = document.querySelector('input.new-todo')
+    // one way to write the query selector
+    // document.querySelector('input.new-todo');
+    // .addEventListener('keydn', function addTodoController(){
+    // })
+    var newTodoInput = document.querySelector('input.new-todo')
 
 
-        // TO ADD NEW TASK:
-        // GIVEN an HTML element <input class="new-todo">
-        // WHEN the user types a task AND presses the "ENTER" key
+    // TO ADD NEW TASK:
+    // GIVEN an HTML element <input class="new-todo">
+    // WHEN the user types a task AND presses the "ENTER" key
 
-        // newTodoInput.addEventListener('keyup', function(){ console.log(arguments);})
-        newTodoInput.addEventListener('keyup', function addTodoController(event){
-                if (event.keyCode === 13){// AND presses the "ENTER" key
+    // newTodoInput.addEventListener('keyup', function(){ console.log(arguments);})
 
-                var task = document.querySelector('input.new-todo').value;
-                todos.taskList.push(task);
-                        document.querySelector('input.new-todo').value= "";
+    newTodoInput.addEventListener('keyup', function addTodoController(event){
+        if (event.keyCode === 13) { // AND presses the "ENTER" key
 
-                    console.log(todos.taskList);
+          var task = document.querySelector('input.new-todo').value;
 
-                    // THEN:
-                    // DONE  Save the thing to remember (task) to the list of things to remember(taskList)
-                    // DONE  Remove the things to remember(task) from the "what needs to be done?"box(input new todo)
-                    // TODO  Update the number of tasks in the footer...
-                    // TODO  Add a new task(ul.todo-list > li) to the list of tasks in the display (ul.todo-list)
-              //  }
+          todos.taskList.push(task);
 
 
-    }); // END addEventListner (addTodoController)
 
-// function lookMaArguments(){ console.log(arguments);}
-    // lookMaArguments();
-    // lookMaArguments(1, 2, 3);
-    // lookMaArguments('hello', 'clarise');
-    // var element = document.querySelector('li');
+					document.querySelector('input.new-todo').value = "";
 
-/**        _.forEach(element, function(element, index, elements){
-            element.addEventListener('click', function(){
-                element.childElement.classList.toggle('checkbox');
-            });
-            });*/
+					if (document.querySelector('span.todo-count').textContent === 1) {
+						else {(todos.taskList.length + "Tasks left"); }
+					}
 
-})(window);
+					document.querySelector('span.todo-count').textContent = (todos.taskList.length + "Tasks Left");
+
+          console.log(todos.taskList);
+
+          // THEN:
+          // DONE  Save the thing to remember (task) to the list of things to remember(taskList)
+          // DONE  Remove the things to remember(task) from the "what needs to be done?"box(input new todo)
+          // TODO  Update the number of tasks in the footer...
+          // TODO  Add a new task(ul.todo-list > li) to the list of tasks in the display (ul.todo-list)
+           }
+
+
+        }); // END addEventListner (addTodoController)
+
+      // function lookMaArguments(){ console.log(arguments);}
+      // lookMaArguments();
+      // lookMaArguments(1, 2, 3);
+      // lookMaArguments('hello', 'clarise');
+      // var element = document.querySelector('li');
+
+      /**        _.forEach(element, function(element, index, elements){
+                  element.addEventListener('click', function(){
+                      element.childElement.classList.toggle('checkbox');
+                  });
+                  });*/
+
+    })(window);
