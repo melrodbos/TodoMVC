8ab7eed636f71d850a730c6162e328c9f04fce14
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-05-17 22:45:43 -0400

    Updating filename

---
+++ todos.js
@@ -0,0 +1,21 @@
+(function(window){
+ var taskList = []
+
+ function listTask(list){
+ }
+
+ function addTask(task, list){
+   return list.push(task);
+ }
+
+ function deleteTask (position, list){
+   return list.splice(position, 1);
+ }
+
+   window.todos = {
+     "taskList": taskList,
+     "addTask": addTask,
+     // "completeTask": completeTask,
+     "deleteTask": deleteTask
+   };
+})(window);
